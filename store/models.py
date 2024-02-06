6320aae6c15e9949ba7ff7b0419c02e4067c36c8
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-06 11:52:51 +0330

    add Order class + use Choice Field in Order class (PAYMENT_STATUS_CHOICES)

--- store/models.py
+++ store/models.py
@@ -25,3 +25,15 @@ class Customer(models.Model):
     phone = models.CharField(max_length=15)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOISES)
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOISES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOISES)
