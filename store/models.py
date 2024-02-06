b75d2b1fd93e4ca4341c06fbf4063477e6f2e546
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-06 11:35:49 +0330

    create models(product+customer)

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=9,decimal_places=3)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    birth_date = models.DateField(null=True)
