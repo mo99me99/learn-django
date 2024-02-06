51a82dba3968833feffd5cfcf795fcd7b0972f7b
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-06 11:44:44 +0330

    use Choice Field in Customer class (MEMBERSHIP_CHOICES)

--- store/models.py
+++ store/models.py
@@ -10,8 +10,18 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    
+    MEMBERSHIP_CHOISES = [
+        (MEMBERSHIP_BRONZE, 'Bronze')
+        (MEMBERSHIP_SILVER, 'SILVER')
+        (MEMBERSHIP_GOLD, 'GOLD')
+    ]
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOISES)
