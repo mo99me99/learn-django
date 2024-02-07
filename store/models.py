514b55ec7b21108fb87035f2d7b7eea731bb5a6f
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-07 11:46:40 +0330

    add zip field to Address class + make migrations + migrate

--- store/models.py
+++ store/models.py
@@ -58,6 +58,7 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    zip = models.CharField(max_length=40)
 
 
 class OrderItem(models.Model):
