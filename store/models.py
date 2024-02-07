89fd4a4fe362e399d046cdeffa8a92097fc49e04
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-07 10:07:39 +0330

    add slug to Product and makemigrations

--- store/models.py
+++ store/models.py
@@ -13,6 +13,7 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=9,decimal_places=3)
     inventory = models.IntegerField()
