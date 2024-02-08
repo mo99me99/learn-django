6dcbb4ce31d66e16b2aaf006431335e46093852b
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-08 10:51:34 +0330

    5- Retrieving Objects

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 
 # Create your views here.
 def say_hello(request):
+    try:
+        product = Product.objects.get(pk=1)
+    except ObjectDoesNotExist:
+        pass
+    
     return render(request, 'hello.html', {'name' : 'Mohammad'})
