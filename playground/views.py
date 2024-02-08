1957c3a900d683c8b2c47091e3a41d363514dc35
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-08 11:47:11 +0330

    Sorting

--- playground/views.py
+++ playground/views.py
@@ -8,5 +8,12 @@ from store.models import Product,Customer,Collection,Order,OrderItem
 # Create your views here.
 def say_hello(request):
     # Products: inventory = price
-    queryset = Product.objects.filter(inventory= F('collection__id'))
+    # queryset = Product.objects.order_by('title')
+    # queryset = Product.objects.order_by('-title')
+    # queryset = Product.objects.order_by('unit_price','title')
+    # queryset = Product.objects.order_by('unit_price','title').reverse()
+    queryset = Product.objects.filter(collection__pk=3).order_by('unit_price')
+    # product = Product.objects.order_by('unit_price')[0]
+    # product = Product.objects.earliest('unit_price')
+
     return render(request, 'hello.html', {'name' : 'Mohammad','products':list(queryset)})
