478448676b2e9d791f242ffe785f39f27a8ed85a
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-08 12:10:04 +0330

    Selecting Fields to Query

--- playground/views.py
+++ playground/views.py
@@ -8,7 +8,10 @@ from store.models import Product,Customer,Collection,Order,OrderItem
 # Create your views here.
 def say_hello(request):
 
-    # queryset = Product.objects.all()[:5]
-    queryset = Product.objects.all()[5:10]
+    # queryset = Product.objects.values('id','title','collection__title')
+    # queryset = Product.objects.values_list('id','title','collection__title')
+    ordered_prods_ids = OrderItem.objects.values('product_id').distinct() #distinct removes dublicates
+    queryset = Product.objects.filter(id__in=ordered_prods_ids).order_by('title')
+
 
     return render(request, 'hello.html', {'name' : 'Mohammad','products':list(queryset)})
