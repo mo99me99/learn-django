45062a25e87c1c62dea0407172412f07a5d5ad72
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-08 11:37:03 +0330

    Referencing Fields using F Objects

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q
+from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product,Customer,Collection,Order,OrderItem
 
 
 # Create your views here.
 def say_hello(request):
-
-    # queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20)) #or
-    # queryset = Product.objects.filter(Q(inventory__lt=10) & Q(unit_price__lt=20)) #and : but no need ot use '&'. you can use filters one after each
-    # queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20)) # x and not(y)
-    
+    # Products: inventory = price
+    queryset = Product.objects.filter(inventory= F('collection__id'))
     return render(request, 'hello.html', {'name' : 'Mohammad','products':list(queryset)})
