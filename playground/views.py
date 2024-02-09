833ff130cdc706ca42136132f8dec484d80d0e77
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-09 18:33:20 +0330

    Transactions (to do entire or none part of a pice of code )

--- playground/views.py
+++ playground/views.py
@@ -1,32 +1,24 @@
 from django.shortcuts import render
+from django.db import transaction
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product, Collection, Cart, CartItem
+from store.models import Product, Collection, Cart, CartItem, Order, OrderItem
 from tags.models import TaggedItem
 
 
 
 # Create your views here.
 def say_hello(request):
-    
-    # • Create a shopping cart with an item 
-    # cart = Cart()
-    # cart.save()
-    # cart_item = CartItem()
-    # cart_item.cart = cart
-    # cart_item.product_id = 11
-    # cart_item.quantity = 10
-    # cart_item.save()
+    # other code 
+    with transaction.atomic():
+        order = Order()
+        order.customer_id=1
+        order.save()
 
-    # • Update the quantity of an item in a shopping cart
-    # item = CartItem.objects.get(pk=1)
-    # item.quantity=2
-    # item.save()
-    
-    # • Remove a shopping cart with its items  
-    # cart = Cart.objects.get(pk=1)
-    # cart.delete()
-
-
-
+        item = OrderItem()
+        item.order = order
+        item.product_id = 1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
 
     return render(request, 'hello.html', {'name' : 'Mohammad','tags':list()})
