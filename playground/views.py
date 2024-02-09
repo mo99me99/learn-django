b1813f231233948cc79c15c38614d20a1fce74b1
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-09 18:08:05 +0330

    do some ex on CRUD db

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product, Collection
+from store.models import Product, Collection, Cart, CartItem
 from tags.models import TaggedItem
 
 
@@ -8,12 +8,25 @@ from tags.models import TaggedItem
 # Create your views here.
 def say_hello(request):
     
-    collection = Collection(pk=11)
-    collection.delete()
+    # • Create a shopping cart with an item 
+    # cart = Cart()
+    # cart.save()
+    # cart_item = CartItem()
+    # cart_item.cart = cart
+    # cart_item.product_id = 11
+    # cart_item.quantity = 10
+    # cart_item.save()
 
-    # collection.objects.filter(id__gt=5).delete()
+    # • Update the quantity of an item in a shopping cart
+    # item = CartItem.objects.get(pk=1)
+    # item.quantity=2
+    # item.save()
     
-    
+    # • Remove a shopping cart with its items  
+    # cart = Cart.objects.get(pk=1)
+    # cart.delete()
+
+
 
 
     return render(request, 'hello.html', {'name' : 'Mohammad','tags':list()})
