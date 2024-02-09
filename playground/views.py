9112babf5c73f68d186835f5238512ad29237650
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-09 17:54:43 +0330

    updating ojects

--- playground/views.py
+++ playground/views.py
@@ -8,16 +8,10 @@ from tags.models import TaggedItem
 # Create your views here.
 def say_hello(request):
     
-    # first approach 
-    # collection = Collection(title='Video Game')
-    collection = Collection()
-    collection.title = 'Video Game'
-    collection.featured_product = Product(pk=1)
-    # collection.featured_product_id = 1 
+    collection = Collection(pk=11)
+    collection.title = 'Game'
+    collection.featured_product = None
     collection.save()
-
-    # second approach
-    # collection = Collection.objects.create(title='Video Game', featured_product_id=1)
 
 
     return render(request, 'hello.html', {'name' : 'Mohammad','tags':list()})
