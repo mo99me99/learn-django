b2726c1da75ab9979ba0a58388dfbdce04321e1b
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-10 23:42:02 +0330

    Adding Computed Columns (inventory_status for ProductAdmin)

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,16 @@ from . import models
 # customize admin model of a class 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory','inventory_status']
     list_editable = ['unit_price']
     list_per_page = 20
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
 
 
 @admin.register(models.Customer)
