f252dfcda30e3d759666a776978898467f90ea81
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-10 23:38:18 +0330

    Customizing the List Page (CustomerAdmin)

--- store/admin.py
+++ store/admin.py
@@ -9,7 +9,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_per_page = 20
 
 
-
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 20
 
 # Register your models here.
 admin.site.register(models.Collection)
