f197e0b189c6d7723ce8efb1a34dc75641cbb7f3
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-11 18:53:08 +0330

    12- Model Serializers (add collection tilte to ProductSerializer fields)

--- store/serializers.py
+++ store/serializers.py
@@ -13,10 +13,14 @@ class CollectionSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price','price_with_tax','description','collection']
+        fields = ['id', 'title', 'unit_price','price_with_tax','description','collection_title']
         # fields = '__all__' #bad practice
 
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    collection_title = serializers.SerializerMethodField(method_name='get_collection_title')
+
+    def get_collection_title(self, product:Product):
+        return product.collection.title
 
     def calculate_tax(self, product : Product):
         return product.unit_price * Decimal(1.1)
