e329646bbf1d42b571d1fa13ccb89edda7a500b1
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-11 16:44:30 +0330

    8- Creating Serializers

---
+++ store/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    unit_price = serializers.DecimalField(max_digits=9,decimal_places=3)
