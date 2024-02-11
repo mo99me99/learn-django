903565679e7e50af8c043941d6340ca0a556fdf3
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-11 18:48:09 +0330

    12- Model Serializers

--- store/serializers.py
+++ store/serializers.py
@@ -4,28 +4,20 @@ from decimal import Decimal
 from store.models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta : 
+        model = Collection
+        fields = ['id', 'title']
 
 
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price','price_with_tax','description','collection']
+        # fields = '__all__' #bad practice
 
-
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(source='unit_price',max_digits=9,decimal_places=3)
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
-    # collection = serializers.PrimaryKeyRelatedField(
-    #     queryset=Collection.objects.all() 
-    # )
-    # collection = serializers.StringRelatedField()
-    # collection = CollectionSerializer()
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name='collection-detail'
-    )
     def calculate_tax(self, product : Product):
         return product.unit_price * Decimal(1.1)
     
