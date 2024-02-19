d07af343101c8a9fcf8e1b4da7e950958e7b0ec2
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-02-19 15:46:21 +0330

    config routes using SimpleRouetr or DefaultRouter + change delete to destroy for destroy an instance (fix a mistake )

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter,DefaultRouter
 from . import views
 
 
+router = DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+
+
+
 # URL configuration
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path("collections/<int:pk>/", views.CollectionDetail.as_view(),name='collection-detail')
-]
+urlpatterns = router.urls
+
+# urlpatterns = [
+    # path('',include(router.urls)) #using this approach you can add other specefic aptterns you wnat and still use router.urls
+    # path('products/', views.ProductList.as_view()),
+    # path('products/<int:pk>/', views.ProductDetail.as_view()),
+    # path('collections/', views.CollectionList.as_view()),
+    # path("collections/<int:pk>/", views.CollectionDetail.as_view(),name='collection-detail')
+# ]
--- store/views.py
+++ store/views.py
@@ -1,49 +1,42 @@
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
-from .models import Collection, Product
+from .models import Collection, OrderItem, Product
 from .serializers import CollectionSerializer, ProductSerializer
 
 
 # Create your views here.
-class ProductViewSet(ReadOnlyModelViewSet):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.select_related('collection').all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {'request', self.request}
-
-    def delete(self, request, pk):
-        product:Product = get_object_or_404(Product, pk=pk)
+    
+    def destroy(self, request, *args, **kwargs):
+        product:Product = get_object_or_404(Product, pk=kwargs['pk'])
         if product.orderitem_set.count() > 0 :
             return Response({'error':'product can not be deleted because it is associated with an order item'}
                             ,status=status.HTTP_405_METHOD_NOT_ALLOWED
                     )
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-
-
-class CollectionViewSet(ReadOnlyModelViewSet):
-    queryset = Collection.objects.annotate(products_count=Count('product')).all()
-    serializer_class = CollectionSerializer
+        return super().destroy(request, *args, **kwargs)
     
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.product_set.count() > 0 :
+
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.annotate(products_count=Count('product')).all()
+    serializer_class = CollectionSerializer
+
+
+    def destroy(self, request, *args, **kwargs):
+        if Collection.objects.filter(product_id=kwargs['pk']).count() > 0 : 
             return Response(
                 {'error':'collection can not be deleted because it is associated with a(some) product(s)'},
                 status=status.HTTP_405_METHOD_NOT_ALLOWED
                 )
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)  
+
+        return super().destroy(request, *args, **kwargs)
