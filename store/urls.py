87fe180c225795a9b4ebc9ad4f5c141e131a1587
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-02-19 13:59:48 +0330

    using generic api view for CollectionList

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
     path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.collection_list),
+    path('collections/', views.CollectionList.as_view()),
     path("collections/<int:pk>/", views.collection_detail,name='collection-detail')
 ]
