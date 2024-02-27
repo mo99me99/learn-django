04697e65bef9909426960f79f531787271747bc3
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-02-27 17:27:47 +0330

    14- Pagination for ALL

--- store/views.py
+++ store/views.py
@@ -18,7 +18,6 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
-    pagination_class = PageNumberPagination
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
 
