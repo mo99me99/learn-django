69b225ba27b1c91ccd3ab72e3474a820dcfbf437
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-02-27 18:04:53 +0330

    add uuid as pk to Cart and set some rules for unique together in CartItem class

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
-
+from uuid import uuid4
 
 
 class Promotion(models.Model):
@@ -128,6 +128,7 @@ class OrderItem(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
@@ -139,6 +140,9 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = ['cart', 'product']
 
 
 class Review(models.Model):
