ac2b3f4ab5aa2e080aa606c0c438e66fffaea784
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-04-05 19:09:04 +0330

    5- Building the Profile API

--- storefront/settings.py
+++ storefront/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -152,10 +153,15 @@ SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
 
-AUTH_USER_MODEL = 'core.User'
 
 DJOSER ={
     'SERIALIZERS':{
         'user_create':'core.serializers.UserCreateSerializer'
     }
 }
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
+
+AUTH_USER_MODEL = 'core.User'
