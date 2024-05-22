e976cbdd17dcee90bcafb95fb7a67d6bf7c51c78
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-05-22 10:35:34 +0330

    5- Attaching Files

--- playground/views.py
+++ playground/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from django.contrib.contenttypes.models import ContentType
 from store.models import Product, Collection, Cart, CartItem, Order, OrderItem
 from tags.models import TaggedItem
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import EmailMessage, BadHeaderError
 
 
 # Create your views here.
@@ -12,7 +12,10 @@ def say_hello(request):
     queryset = Product.objects.all()
     try:
         # send_mail('subject', 'message', 'info@mo.com', ['m.m.hosseini1099@gmail.com'])
-        mail_admins('subject', 'message', html_message='message')
+        # mail_admins('subject', 'message', html_message='message')
+        message = EmailMessage('subject', 'message', 'from@mobuy.com', ['m.m.hosseini1099@gmail.com'] )
+        message.attach_file('playground/static/images/DES.png')
+        message.send()
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name' : 'Mohammad'})
