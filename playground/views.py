ab54bc818a19e5499379b3f844fbe1676bed3a67
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-06-06 10:15:36 +0330

    4- Simulating a Slow API

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 from django.db import connection
-from django.http import JsonResponse
-from django.contrib.contenttypes.models import ContentType
-from store.models import Product, Collection, Cart, CartItem, Order, OrderItem
-from tags.models import TaggedItem
-from .tasks import notify_customers
-
+import requests
 
 # Create your views here.
 def say_hello(request):
-    
-    notify_customers.delay('Hello')
-    return render(request, 'hello.html', {'name' : 'Mohammad'})
+    requests.get('http://127.0.0.1:8001/play/delay/')
+    return render(request, 'hello.html', {'name':'mohammad'})
