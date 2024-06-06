10e1bc0339d29799d8f6cb753a0499cc86f801ff
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-06-06 10:42:57 +0330

    8- Using the Low-level Cache API

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.db import connection
+from django.core.cache import cache
 import requests
 
 # Create your views here.
 def say_hello(request):
-    requests.get('http://127.0.0.1:8001/play/delay/')
-    return render(request, 'hello.html', {'name':'mohammad'})
+    key = 'httpbin_result'
+    if cache.get(key) is None:
+        response = requests.get('http://127.0.0.1:8001/play/delay/')
+        data = response.json()
+        cache.set(key, data)
+    return render(request, 'hello.html', {'name':cache.get(key)})
