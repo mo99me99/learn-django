49f75baf3ed2929ed174e54953cd1dac811e2dd1
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-06-06 10:47:38 +0330

    9- Caching Views (functional view)

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.db import connection
 from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 import requests
 
 # Create your views here.
+@cache_page(5 * 60)
 def say_hello(request):
-    key = 'httpbin_result'
-    if cache.get(key) is None:
-        response = requests.get('http://127.0.0.1:8001/play/delay/')
-        data = response.json()
-        cache.set(key, data)
-    return render(request, 'hello.html', {'name':cache.get(key)})
+    response = requests.get('http://127.0.0.1:8001/play/delay/')
+    data = response.json()
+    return render(request, 'hello.html', {'name':data})
+
