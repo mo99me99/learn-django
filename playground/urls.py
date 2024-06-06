e599cd0c5ff1474b29f80e7d9bd39d12d365129d
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-06-06 10:52:14 +0330

    9- Caching Views (class based view)

--- playground/urls.py
+++ playground/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 # URL configuration
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('hello/', views.HelloView.as_view())
 ]
--- playground/views.py
+++ playground/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.db import connection
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
 import requests
 
 # Create your views here.
-@cache_page(5 * 60)
-def say_hello(request):
-    response = requests.get('http://127.0.0.1:8001/play/delay/')
-    data = response.json()
-    return render(request, 'hello.html', {'name':data})
 
+class HelloView(APIView):
+
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+        response = requests.get('http://127.0.0.1:8001/play/delay/')
+        data = response.json()
+        return render(request, 'hello.html', {'name':data})
+
+        
